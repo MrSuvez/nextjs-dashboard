fe654251a4e11a550fdea2393da6b451b58d1f3c
Suveer Prithipal <suveer.prithipal@serko.com>
2025-03-24 22:08:54 +1300

    ups

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
             I have been a software Engineer for over 20 years and have a rich history of working with React, Next.js, and other web technologies.<br/><br/>
             In this example you will find frontend development, backend api implementation, postgres database and authenthication.<br/><br/>
             I am a founder, creating and IOT startup, having experience designing hardware, firmware, launcing a mobile, shopping and software.<br/><br/>
-            I have real world expereince bringing an idea to life, compliance and regulation, design and manufacturing.<br/><br/>
+            I have real world expereince commercializing products, going through compliance and regulation, design and manufacturing.<br/><br/>
             I am also a mentor, helping others to learn and grow in the software industry.<br/>
             I am a consultant, helping individuals and companies to build and grow their brand and products.<br/><br/>
             Having a passion for vertical and indoor farming, bringing technologies and farming together. 
