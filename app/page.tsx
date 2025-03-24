52d7fdecb4e8eb32f18e7496435c068623034fb0
Suveer Prithipal <suveer.prithipal@serko.com>
2025-03-24 22:06:00 +1300

    complete

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,18 @@ export default function Page() {
         <div
   />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome.</strong> This is the example for the Next.js course built by Suveer P. Contact me for login details.
+            <strong>Welcome.</strong> <br/>
+            This is an example for the Next.js App Router built by Suveer P. Contact me for login details.<br/>
             <a href="https://nz.linkedin.com/in/suveerp" className="text-blue-500">
-              
-            </a>
+              Suveer P LinkedIn
+            </a> <br/><br/>
+            I have been a software Engineer for over 20 years and have a rich history of working with React, Next.js, and other web technologies.<br/><br/>
+            In this example you will find frontend development, backend api implementation, postgres database and authenthication.<br/><br/>
+            I am a founder, creating and IOT startup, having experience designing hardware, firmware, launcing a mobile, shopping and software.<br/><br/>
+            I have real world expereince bringing an idea to life, compliance and regulation, design and manufacturing.<br/><br/>
+            I am also a mentor, helping others to learn and grow in the software industry.<br/>
+            I am a consultant, helping individuals and companies to build and grow their brand and products.<br/><br/>
+            Having a passion for vertical and indoor farming, bringing technologies and farming together. 
           </p>
           <Link
             href="/login"
